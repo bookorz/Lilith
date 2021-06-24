f56c3970434fed8469ec269bbd44c2a781661032
pingchung_kao <pingchung_kao@sanwa-eng.com.tw>
2021-06-24 09:32:20 +0800

    1.版本號更新

--- WindowsFormsApp1/FormVersion.cs
+++ WindowsFormsApp1/FormVersion.cs
@@ -29,7 +29,7 @@ namespace GUI
             //{
             //    Ver_lb.Text = "Version: 2.1.1.5(2018)";
             //}
-            Ver_lb.Text = "Version: 3.09(2020)";
+            Ver_lb.Text = "Version: 3.10(2020)";
         }
     
     }
